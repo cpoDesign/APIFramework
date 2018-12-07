28ac6ded22ca569fc7d5852b091f5ffee3e501d8
pav <pvl.svarc@gmail.com>
2018-12-07 20:34:45 +0000

    Removed unused tests

--- CPODesign.ApiFramework.Tests.Unit/ApiWrapperTests.cs
+++ CPODesign.ApiFramework.Tests.Unit/ApiWrapperTests.cs
@@ -151,20 +151,6 @@ namespace ConsoleApp1.Tests.Unit
             Assert.AreEqual("application/xml", api.DefaultResponseType);
         }
 
-        //[TestMethod]
-        //public void SetEndPointUrl_PassValidString_ShouldPass()
-        //{
-        //    const string restaurant = "restaurant";
-        //    const string baseUrl = "https://www.dummy.com/";
-
-        //    string ParsedUrl = $"{baseUrl}{restaurant}";
-        //    var apiWrappper = new ApiWrapper()
-        //        .SetBaseUrl(baseUrl)
-        //        .SetEndpointUrl(restaurant);
-
-        //    Assert.AreEqual(ParsedUrl.ToString(), apiWrappper..ToString());
-        //}
-
         [TestMethod]
         public void AddCustomHeaders_PassNullParamenter_ShouldReturnArgumentNullException()
         {
@@ -175,22 +161,6 @@ namespace ConsoleApp1.Tests.Unit
         public void AddCustomHeader_PassEmptyNameParamenter_ShouldReturnArgumentNullException()
         {
             Assert.ThrowsException<ArgumentException>(() => new ApiWrapper().AddCustomHeader(new CustomHeader(string.Empty, string.Empty)));
-        }
-
-        [Ignore]
-        [TestMethod]
-        public void AddCustomHeader_PassInvalidCustomHeader_ShouldReturnArgumentNullException()
-        {
-            //var wrapper = new ApiWrapper().AddCustomHeaders(new List<CustomHeaders>);
-
-            /*
-             *  if (headers == null)
-            {
-                throw new ArgumentNullException("Please provide headers", nameof(headers));
-            }
-
-            return this;
-            */
         }
     }
 }
